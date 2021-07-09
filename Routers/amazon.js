2b49e9fcfc63abd94a22fc4daa3b6d0875cb1cd8
Hamid Nawaz <hamid.nawaz28@yahoo.com>
2021-07-09 07:26:42 -0700

    Folder org

--- Routers/amazon.js
+++ Routers/amazon.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-let amazonScrap = require("../Helper/Amazon");
+let amazonScrap = require("../Helper/amazon");
 router.get("/", async function (req, res) {
   const q = req.query;
   res.status(200).send(await amazonScrap.firstOne(q));
