1fc106a93bce26c5c260260c1c42d1bc12c14c39
Hamid Nawaz <hamid.nawaz28@yahoo.com>
2021-07-09 08:15:57 -0700

    Fixing

--- scraps/wallmart/walmart.js
+++ scraps/wallmart/walmart.js
@@ -1,18 +1,14 @@
-"use strict";
-
 const puppeteer = require("puppeteer-extra");
 const pluginStealth = require("puppeteer-extra-plugin-stealth");
 puppeteer.use(pluginStealth());
 
-const AMAZON = {
-  async firstOne(q) {
-    let queries = Object.keys(q)
-      .map((item) => `&${item}=${q[item]}`.split(" ").join("+"))
-      .join("");
+const WALMART = {
+  async firstOne(query, filter, currentPage) {
     let browser = "";
     try {
       browser = await puppeteer.launch({
-        headless: true,
+        headless: false,
+
         args: [
           "--no-sandbox",
           "--disable-setuid-sandbox",
@@ -21,13 +17,20 @@ const AMAZON = {
           "--lang=en-US;q=0.9,en",
         ],
       });
+
       const page = await browser.newPage();
       page.setViewport({
         width: 1400,
         height: 1050,
       });
-      let url = `https://www.amazon.com/s?${queries}`;
+      await page.goto("https://www.walmart.com/ ", {
+        waitUntil: "load",
+        timeout: 0,
+      });
+      let q = query.split(" ").join("%20");
+      let url = `https://www.walmart.com/search/?query=${q}  `;
       console.log("Url--------", url);
+
       await page.goto(url, {
         waitUntil: "load",
         timeout: 0,
@@ -36,7 +39,6 @@ const AMAZON = {
         let product = await page.evaluate(this.extractData);
         console.log("extracted data=== ", product);
         if (product) {
-          return product;
         }
       } catch (err) {
         console.log("error =>", err);
@@ -53,33 +55,41 @@ const AMAZON = {
   },
   extractData() {
     let out = [];
-    Array.from(
-      document.querySelectorAll(".a-section.a-spacing-medium")
-    ).forEach((item) => {
-      var imag = item.querySelector("img")?.src;
-      let title = item.querySelector(".a-link-normal.a-text-normal")?.innerText;
-      let rating = item.querySelector(".a-icon-alt")?.innerText;
-      let totalRating = item.querySelector(".a-size-base")?.innerText;
-      let price = item.querySelector(".a-price span.a-offscreen")?.innerText;
+
+    let allProducts = document.querySelectorAll("li.Grid-col");
+    Array.from(allProducts).forEach((item) => {
+      var imag = item.querySelector(".orientation-portrait img")?.src;
+      let title = item
+        .querySelector(".orientation-portrait img")
+        ?.getAttribute("alt");
+      let rating = item.querySelector(
+        ".visuallyhidden.seo-avg-rating"
+      )?.innerText;
+      let totalRating = item
+        .querySelector(".stars-reviews-count")
+        ?.innerText.replace("\nratings", "");
+      let price = item.querySelector(
+        ".price.display-inline-block.arrange-fit.price.price-main > .visuallyhidden"
+      )?.innerText;
       let link = item.querySelector(
         "a-size-base.a-link-normal.a-text-normal"
       )?.innerText;
-
       let ob = {
         imageUrl: imag,
         title,
         price,
         rating: rating?.replace(" out of 5 stars", ""),
-        // ratings: totalRating,
+        ratings: totalRating,
         link,
       };
       out.push(ob);
       ob = {};
     });
     out = out.filter((item) => item.imageUrl && item.title);
+    // Return scraped data
     return out;
   },
 };
-module.exports = AMAZON;
+module.exports = WALMART;
 
-// AMAZON.firstOne('shoe nike new', 'price-asc-rank', '25');
+// WALMART.firstOne('men boots', '', '');
