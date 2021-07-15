97e0da3c20051a772abcb296b3c1a8dffe634ac0
Hamid Nawaz <hamid.nawaz28@yahoo.com>
2021-07-15 01:53:41 -0700

    Added amazon sku

--- routers/amazon/amazon.js
+++ routers/amazon/amazon.js
@@ -1,8 +1,14 @@
 var express = require("express");
 var router = express.Router();
 let amzScrp = require("../../scraps/amazon/amazon");
+let amzScrpSku = require("../../scraps/amazon/amazonSku");
+
 router.get("/", async function (req, res) {
   const q = req.query;
   res.status(200).send(await amzScrp.firstOne(q));
+});
+router.get("/:id", async function (req, res) {
+  const sku = req.params.id;
+  res.status(200).send(await amzScrpSku.firstOne(sku));
 });
 module.exports = router;
