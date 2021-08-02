38b561039a67b1465e39d3b7736db617bdd90f76
Hamid Nawaz <hamid.nawaz28@yahoo.com>
2021-08-02 04:35:03 -0700

    Added brands, varients and dimensions

--- scraps/amazon/amazonSku.js
+++ scraps/amazon/amazonSku.js
@@ -3,12 +3,11 @@ const puppeteer = require("puppeteer-extra");
 const pluginStealth = require("puppeteer-extra-plugin-stealth");
 puppeteer.use(pluginStealth());
 const sleep = (duration) =>
-new Promise((resolve) => setTimeout(resolve, duration));
+  new Promise((resolve) => setTimeout(resolve, duration));
 const msleep = 2000; // sleeping time
 
 const AMAZON = {
   async firstOne(sku) {
-
     let browser = "";
     try {
       browser = await puppeteer.launch({
@@ -20,7 +19,6 @@ const AMAZON = {
           "--disable-dev-shm-usage",
           "--lang=en-US;q=0.9,en",
           "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36",
-
         ],
       });
       const page = await browser.newPage();
@@ -28,7 +26,7 @@ const AMAZON = {
         width: 1400,
         height: 1050,
       });
-  
+
       await page.goto("https://www.amazon.com/", {
         waitUntil: "load",
         timeout: 0,
@@ -53,7 +51,7 @@ const AMAZON = {
           let product = await page.evaluate(this.extractData);
           console.log("extracted data=== ", product);
           if (product) {
-            return product
+            return product;
           }
         } catch (err) {
           console.log("error =>", err);
@@ -88,21 +86,25 @@ const AMAZON = {
         .split(":")[1]
         .replace(/[" " ]+/g, "");
     }
-    
+
     // Extract title
     let productTitle = document.querySelector("#productTitle");
     if (productTitle) {
       data["title"] = productTitle.innerText.trim();
     }
-   
+
     //Extract Category
-    let prodCategory= Array.from(document.querySelectorAll("#wayfinding-breadcrumbs_feature_div li"))
-    if(prodCategory){
-      data["category"]=prodCategory.map(item=>item.innerText).filter(item=>item!="›")
+    let prodCategory = Array.from(
+      document.querySelectorAll("#wayfinding-breadcrumbs_feature_div li")
+    );
+    if (prodCategory) {
+      data["category"] = prodCategory
+        .map((item) => item.innerText)
+        .filter((item) => item != "›");
     }
-    
+
     // Extract description
-    data["description"]='';
+    data["description"] = "";
     let productDescription = document.querySelector("#productDescription >p");
     if (productDescription) {
       try {
@@ -114,15 +116,19 @@ const AMAZON = {
 
     // Extract Product images
     data["main_image"] = [];
-    let productImg = Array.from(document.querySelectorAll(".a-unordered-list.a-nostyle.a-horizontal.list.maintain-height img"))
+    let productImg = Array.from(
+      document.querySelectorAll(
+        ".a-unordered-list.a-nostyle.a-horizontal.list.maintain-height img"
+      )
+    );
     if (productImg) {
-      let extractedImg = productImg.map(item=>item.src);
+      let extractedImg = productImg.map((item) => item.src);
       data["main_image"].push(extractedImg);
     }
 
     // data["all_images"]=''
     // let AllImg =
-    
+
     // Extract Product Sizes Available
     data["availSizes"] = [];
     let productSize = document.querySelectorAll(
@@ -140,7 +146,7 @@ const AMAZON = {
       let rating = prodRating.innerText.replace(" out of 5 stars", "");
       data["stars"] = rating;
     }
-   
+
     // Extract price
     data["offerPrice"] = [];
     let prodPrice = document.querySelector("#priceblock_ourprice");
@@ -160,7 +166,7 @@ const AMAZON = {
         data["mainPrice"] = "";
       }
     }
-    
+
     // Extract color
     data["color"] = [];
     let prodColor = document.querySelector(
@@ -177,41 +183,87 @@ const AMAZON = {
 
     // Retailer
     data["retailer"] = "Amazon";
-    
+
+    // Brand
+    data["brand"] = document
+      ?.querySelector("#bylineInfo")
+      ?.innerText.replace(/[Bb]rand:?\s?/, "")
+      ?.replace(/Visit the\s?/, "")
+      ?.replace(/\s?[Ss]tore/, "");
+
     // Check if prime
-    let prime = document.querySelector("._multi-card-creative-desktop_DesktopGridColumn_gridColumn__2Jfab > div > a")?.getAttribute("aria-label").includes("Eligible for Prime");
-    if(prime){
-        data["Prime"]=true;
-      }
-      else{
-        data["Prime"]=false;
-      }
+    let prime = document
+      .querySelector(
+        "._multi-card-creative-desktop_DesktopGridColumn_gridColumn__2Jfab > div > a"
+      )
+      ?.getAttribute("aria-label")
+      .includes("Eligible for Prime");
+    if (prime) {
+      data["Prime"] = true;
+    } else {
+      data["Prime"] = false;
+    }
 
-     // Feature Bullets
-     let featureB = Array.from(document.querySelectorAll("#feature-bullets span "))
-     if(featureB){
-      data["FeatureBullets"]=featureB.map(item=>item.innerText)
-     }
+    // Feature Bullets
+    let featureB = Array.from(
+      document.querySelectorAll("#feature-bullets span ")
+    );
+    if (featureB) {
+      data["FeatureBullets"] = featureB.map((item) => item.innerText);
+    }
 
-     // Package Dimensions
-     let PackgDimenstion = document.querySelector("#detailBulletsWrapper_feature_div")
+    // Package Dimensions
+    let PackgDimenstion = document.querySelector(
+      "#detailBulletsWrapper_feature_div"
+    );
 
-     if(PackgDimenstion){
-         data["PackageDimention"]=PackgDimenstion?.innerText;
-       }
-       else{
-         data["PackageDimention"]='';
-       }
+    if (PackgDimenstion) {
+      data["PackageDimention"] = PackgDimenstion?.innerText;
+    } else {
+      data["PackageDimention"] = "";
+    }
 
-      //Variants...
+    // Measurements
+    let measurementArray = Array.from(
+      document?.querySelectorAll("#detailBullets_feature_div li span")
+    )
+      ?.filter((item) => item?.innerText?.includes("Package Dimensions"))?.[0]
+      ?.querySelector("span:nth-child(2)")
+      ?.innerText.split(";");
+    const [dimensions, weight] = measurementArray;
+    data["dimensionUnit"] = dimensions.match(/[a-zA-Z]{2,}/)?.[0];
+    [data["length"], data["width"], data["height"]] = dimensions
+      ?.replace(/\s?[a-zA-Z]{2,}/, "")
+      ?.split(" x ");
+    data["weight"] = weight?.match(/[0-9.]+/)?.[0];
+    data["weightUnit"] = weight?.match(/[a-zA-Z]+/)?.[0];
 
-
+    //Variants...
+    let varientData = Array.from(document.querySelectorAll("[type]")).filter(
+      (item) => item.innerText.includes("twister-js-init-dpx-data")
+    )[0].innerText;
+    let varients = JSON.parse(
+      varientData
+        .match(
+          /"dimensionValuesDisplayData"\s+:\s{["0-9A-Za-z/\[/\]:,\s.]+}/
+        )[0]
+        .replace(/"dimensionValuesDisplayData"\s+:\s/, "")
+    );
+    data["all_variants"] = Object.keys(varients).map((item) => {
+      return {
+        product_id: item,
+        variant_specifics: [
+          { dimension: "Size", value: varients[item][0] },
+          { dimension: "Color", value: varients[item][1] },
+        ],
+      };
+    });
     return data;
   },
 };
 module.exports = AMAZON;
 
-AMAZON.firstOne('B07T3P4ZB4');
+AMAZON.firstOne("B07T3P4ZB4");
 // AMAZON.firstOne('B08X75NTSX');
 // AMAZON.firstOne('B0963R7SJN');
 // AMAZON.firstOne('', 'B07HRC68QF', 'test');
